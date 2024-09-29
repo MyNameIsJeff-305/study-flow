fb7cd277c99d429eafc903b29159c31c86aef76b
MyNameIsJeff-305 <michelgarcia950528@icloud.com>
2024-09-28 23:24:11 -0400

    HEy

--- backend/app.js
+++ backend/app.js
@@ -1,36 +1,39 @@
 const express = require('express');
-require('express-async-errors');
 const morgan = require('morgan');
 const cors = require('cors');
 const csurf = require('csurf');
 const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
-const { environment } = require('./config');
-const isProduction = environment === 'production';
 const { ValidationError } = require('sequelize');
+require('express-async-errors');
+
+
+const { environment } = require('./config');
 const routes = require('./routes');
 
+
+const isProduction = environment === 'production';
+
+
 const app = express();
-app.use(morgan('dev'));
+
+
+app.use(morgan('dev'));  // logging
+
+
 app.use(cookieParser());
-app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 
-// Security Middleware
 if (!isProduction) {
-    // enable cors only in development
     app.use(cors());
 }
 
-// helmet helps set a variety of headers to better secure your app
-app.use(
-    helmet.crossOriginResourcePolicy({
-        policy: "cross-origin"
-    })
+app.use(helmet.crossOriginResourcePolicy({
+    policy: "cross-origin"
+})
 );
 
-// Set the _csrf token and create req.csrfToken method
 app.use(
     csurf({
         cookie: {
@@ -41,23 +44,19 @@ app.use(
     })
 );
 
+
 app.use(routes);
 
-// Catch unhandled requests and forward to error handler.
+
 app.use((_req, _res, next) => {
-    // console.log(_req._parsedOriginalUrl.pathname, " ^^^^^^")
     const err = new Error("The requested resource couldn't be found.");
     err.title = "Resource Not Found";
     err.errors = { message: "The requested resource couldn't be found." };
     err.status = 404;
-    next(err);
+    next(err.errors);
 });
 
-
-
-// Process sequelize errors
 app.use((err, _req, _res, next) => {
-    // check if error is a Sequelize error:
     if (err instanceof ValidationError) {
         let errors = {};
         for (let error of err.errors) {
@@ -69,16 +68,21 @@ app.use((err, _req, _res, next) => {
     next(err);
 });
 
-// Error formatter
-app.use((err, _req, res, _next) => {
+app.use((err, _req, res, _next) => {  // error formatter
     res.status(err.status || 500);
+    if (isProduction) {
+        delete err.title;
+        delete err.stack;
+    }
     console.error(err);
     res.json({
-        title: err.title || 'Server Error',
+        // title: err.title || 'Server Error',
         message: err.message,
         errors: err.errors,
-        stack: isProduction ? null : err.stack
+        // stack: isProduction ? null : err.stack
     });
 });
+
+//Fixed the 'title' and 'stack' problems in production
 
 module.exports = app;
