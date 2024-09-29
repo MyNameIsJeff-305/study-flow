e33b3b420b1b4cee8441bd24fe9325c20fa233aa
MyNameIsJeff-305 <michelgarcia950528@icloud.com>
2024-09-28 23:00:59 -0400

    Is this working?

--- backend/db/models/exercise.js
+++ backend/db/models/exercise.js
@@ -18,7 +18,7 @@ module.exports = (sequelize, DataTypes) => {
       type: DataTypes.INTEGER,
       allowNull: false,
       references: {
-        model: 'StudyPlans',
+        model: 'Studyplans',
         key: 'id'
       },
       onDelete: 'CASCADE'
--- backend/db/seeders/02-demo-studyplan.js
+++ backend/db/seeders/02-demo-studyplan.js
@@ -1,7 +1,7 @@
 'use strict';
 
 let options = {};
-options.tableName = 'StudyPlans';
+options.tableName = 'Studyplans';
 
 if (process.env.NODE_ENV === 'production') {
   options.schema = process.env.SCHEMA;
